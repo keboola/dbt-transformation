e73acdbbb834400b54951351434d7b6514f4ee7c
Výborný Adam <mail@adamvyborny.cz>
2022-02-01 09:49:26 +0100

    test IO

--- src/Component.php
+++ src/Component.php
@@ -10,7 +10,12 @@ class Component extends BaseComponent
 {
     protected function run(): void
     {
-        // @TODO implement
+        $path    = __DIR__ . '../data/in/tables';
+        $files = array_diff(scandir($path), ['.', '..']);
+        echo 'input tables loaded:' . PHP_EOL;
+        foreach ($files as $file) {
+            echo $file . PHP_EOL;
+        }
     }
 
     protected function getConfigClass(): string

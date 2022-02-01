1709462b73e70b9322eefc65e792b4d284cde40e
Výborný Adam <mail@adamvyborny.cz>
2022-02-01 11:12:06 +0100

    phpstan fix

--- src/Component.php
+++ src/Component.php
@@ -11,7 +11,7 @@ class Component extends BaseComponent
     protected function run(): void
     {
         $path    = __DIR__ . '../data/in/tables';
-        $files = array_diff(scandir($path), ['.', '..']);
+        $files = array_diff(scandir($path) ?: [], ['.', '..']);
         echo 'input tables loaded:' . PHP_EOL;
         foreach ($files as $file) {
             echo $file . PHP_EOL;

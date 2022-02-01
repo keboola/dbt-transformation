6aa6ffb53c0bf3d90ad1ffd2aef5e63d0d819995
Výborný Adam <mail@adamvyborny.cz>
2022-02-01 12:54:20 +0100

    fix phpcs

--- src/run.php
+++ src/run.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
+use DbtTransformation\Component;
 use Keboola\Component\Logger;
 use Keboola\Component\UserException;
-use DbtTransformation\Component;
 
 require __DIR__ . '/../vendor/autoload.php';
 

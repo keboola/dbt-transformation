d46790d3441023c619ed8c6db6be36ce6fc906eb
Výborný Adam <mail@adamvyborny.cz>
2022-02-03 19:15:17 +0100

    Fix PHPCS

--- src/Component.php
+++ src/Component.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace DbtTransformation;
 
 use Keboola\Component\BaseComponent;
+use Keboola\Component\UserException;
 use Psr\Log\LoggerInterface;
+use RuntimeException;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Exception\ProcessFailedException;
@@ -22,13 +24,20 @@ class Component extends BaseComponent
         $this->filesystem = new Filesystem();
     }
 
+    /**
+     * @throws \Keboola\Component\UserException
+     */
     protected function run(): void
     {
         $dataDir = $this->getDataDir();
         $config = $this->getConfig();
         $gitRepositoryUrl = $config->getGitRepositoryUrl();
 
-        $this->runProcessInDataDir(['git', 'clone', $gitRepositoryUrl]);
+        try {
+            $this->runProcessInDataDir(['git', 'clone', $gitRepositoryUrl]);
+        } catch (ProcessFailedException $e) {
+            throw new UserException(sprintf('Failed to clone your repository: %s', $gitRepositoryUrl));
+        }
 
         $projectPath = $this->getProjectPath($dataDir, $gitRepositoryUrl);
         $this->addProfileYamlToProject($projectPath, $config->getAuthorization()['workspace']);
@@ -38,7 +47,7 @@ class Component extends BaseComponent
             echo $file . PHP_EOL;
         }
 
-        readfile(sprintf("%s/.dbt/profile.yml", $projectPath));
+        readfile(sprintf('%s/.dbt/profile.yml', $projectPath));
     }
 
     public function getConfig(): Config
@@ -58,6 +67,9 @@ class Component extends BaseComponent
         return ConfigDefinition::class;
     }
 
+    /**
+     * @param string[] $workspace
+     */
     protected function runProcessInDataDir(array $command): Process
     {
         $process = new Process($command, $this->getDataDir());
@@ -66,32 +78,41 @@ class Component extends BaseComponent
         if (!$process->isSuccessful()) {
             throw new ProcessFailedException($process);
         }
+
         return $process;
     }
 
     protected function createDbtFolderIfNotExist(string $projectPath): void
     {
-        $dbtFolderPath = sprintf("%s/.dbt", $projectPath);
+        $dbtFolderPath = sprintf('%s/.dbt', $projectPath);
         try {
             if (!$this->filesystem->exists($dbtFolderPath)) {
                 $this->filesystem->mkdir($dbtFolderPath);
             }
         } catch (IOExceptionInterface $e) {
-            echo sprintf('An error occurred while creating directory %s', $dbtFolderPath);
+            throw new RuntimeException(sprintf('An error occurred while creating directory %s', $dbtFolderPath));
         }
     }
 
+    /**
+     * @param string[] $workspace
+     * @throws \Keboola\Component\UserException
+     */
     protected function addProfileYamlToProject(string $projectPath, array $workspace): void
     {
         $this->createDbtFolderIfNotExist($projectPath);
-        $dbtProjectYaml = Yaml::parseFile($projectPath . '/dbt_project.yml');
+        $dbtProjectYamlPath = $projectPath . '/dbt_project.yml';
+        if (!$this->filesystem->exists($dbtProjectYamlPath)) {
+            throw new UserException(sprintf('Missing file %s in your project', $dbtProjectYamlPath));
+        }
+        $dbtProjectYaml = Yaml::parseFile($dbtProjectYamlPath);
         $this->filesystem->dumpFile(
-            sprintf("%s/.dbt/profile.yml", $projectPath),
+            sprintf('%s/.dbt/profile.yml', $projectPath),
             Yaml::dump([
                 $dbtProjectYaml['profile'] => [
                     'target' => 'dev',
                     'outputs' => ['dev' => $workspace],
-                ]
+                ],
             ], 4)
         );
     }
@@ -99,6 +120,6 @@ class Component extends BaseComponent
     protected function getProjectPath(string $dataDir, string $gitRepositoryUrl): string
     {
         $explodedUrl = explode('/', $gitRepositoryUrl);
-        return sprintf("%s/%s", $dataDir, pathinfo(end($explodedUrl), PATHINFO_FILENAME));
+        return sprintf('%s/%s', $dataDir, pathinfo(end($explodedUrl), PATHINFO_FILENAME));
     }
 }

0f858d1f03592c540448137b6dccc3b0a0185e46
Výborný Adam <mail@adamvyborny.cz>
2022-02-03 18:53:18 +0100

    Generate profile.yml from credentials

--- src/Component.php
+++ src/Component.php
@@ -5,16 +5,40 @@ declare(strict_types=1);
 namespace DbtTransformation;
 
 use Keboola\Component\BaseComponent;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 use Symfony\Component\Yaml\Yaml;
 
 class Component extends BaseComponent
 {
+    protected Filesystem $filesystem;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        parent::__construct($logger);
+        $this->filesystem = new Filesystem();
+    }
+
     protected function run(): void
     {
-        $yaml = Yaml::dump($this->getConfig()->getAuthorization());
-        file_put_contents($this->getDataDir() . '/out/files/profile.yaml', $yaml);
+        $dataDir = $this->getDataDir();
+        $config = $this->getConfig();
+        $gitRepositoryUrl = $config->getGitRepositoryUrl();
 
-        echo $this->getConfig()->getGitRepositoryUrl();
+        $this->runProcessInDataDir(['git', 'clone', $gitRepositoryUrl]);
+
+        $projectPath = $this->getProjectPath($dataDir, $gitRepositoryUrl);
+        $this->addProfileYamlToProject($projectPath, $config->getAuthorization()['workspace']);
+
+        $files = array_diff(scandir($projectPath) ?: [], ['.', '..']);
+        foreach ($files as $file) {
+            echo $file . PHP_EOL;
+        }
+
+        readfile(sprintf("%s/.dbt/profile.yml", $projectPath));
     }
 
     public function getConfig(): Config
@@ -32,5 +56,49 @@ class Component extends BaseComponent
     protected function getConfigDefinitionClass(): string
     {
         return ConfigDefinition::class;
+    }
+
+    protected function runProcessInDataDir(array $command): Process
+    {
+        $process = new Process($command, $this->getDataDir());
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+        return $process;
+    }
+
+    protected function createDbtFolderIfNotExist(string $projectPath): void
+    {
+        $dbtFolderPath = sprintf("%s/.dbt", $projectPath);
+        try {
+            if (!$this->filesystem->exists($dbtFolderPath)) {
+                $this->filesystem->mkdir($dbtFolderPath);
+            }
+        } catch (IOExceptionInterface $e) {
+            echo sprintf('An error occurred while creating directory %s', $dbtFolderPath);
+        }
+    }
+
+    protected function addProfileYamlToProject(string $projectPath, array $workspace): void
+    {
+        $this->createDbtFolderIfNotExist($projectPath);
+        $dbtProjectYaml = Yaml::parseFile($projectPath . '/dbt_project.yml');
+        $this->filesystem->dumpFile(
+            sprintf("%s/.dbt/profile.yml", $projectPath),
+            Yaml::dump([
+                $dbtProjectYaml['profile'] => [
+                    'target' => 'dev',
+                    'outputs' => ['dev' => $workspace],
+                ]
+            ], 4)
+        );
+    }
+
+    protected function getProjectPath(string $dataDir, string $gitRepositoryUrl): string
+    {
+        $explodedUrl = explode('/', $gitRepositoryUrl);
+        return sprintf("%s/%s", $dataDir, pathinfo(end($explodedUrl), PATHINFO_FILENAME));
     }
 }

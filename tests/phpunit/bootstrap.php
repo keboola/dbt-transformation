f9ecc6f454871cc91ddfc5452502d11d5dc0c6a9
Výborný Adam <mail@adamvyborny.cz>
2022-02-14 10:31:34 +0100

    Installed and configured Snowflake connection

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -3,3 +3,14 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createUnsafeMutable(__DIR__ . '/../../');
+$dotenv->safeLoad();
+$dotenv->required([
+    'SNOWFLAKE_HOST',
+    'SNOWFLAKE_WAREHOUSE',
+    'SNOWFLAKE_DATABASE',
+    'SNOWFLAKE_SCHEMA',
+    'SNOWFLAKE_USER',
+    'SNOWFLAKE_PASSWORD',
+]);

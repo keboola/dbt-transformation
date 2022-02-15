75a1e380cb7fa554ac7ebe20a9644ee07e6c094f
Výborný Adam <mail@adamvyborny.cz>
2022-02-15 11:45:14 +0100

    Changed env variables validation to work in CI

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -6,11 +6,18 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createUnsafeMutable(__DIR__ . '/../../');
 $dotenv->safeLoad();
-$dotenv->required([
+
+$environments = [
     'SNOWFLAKE_HOST',
     'SNOWFLAKE_WAREHOUSE',
     'SNOWFLAKE_DATABASE',
     'SNOWFLAKE_SCHEMA',
     'SNOWFLAKE_USER',
     'SNOWFLAKE_PASSWORD',
-]);
+];
+
+foreach ($environments as $environment) {
+    if (empty(getenv($environment))) {
+        throw new RuntimeException(sprintf('Missing environment "%s".', $environment));
+    }
+}

ef9811ea1e071fe167ae73f33a6f07d8c9297ad4
Výborný Adam <mail@adamvyborny.cz>
2022-02-15 11:32:04 +0100

    Added new parameters to config and covered with test

--- src/ConfigDefinition.php
+++ src/ConfigDefinition.php
@@ -6,6 +6,7 @@ namespace DbtTransformation;
 
 use Keboola\Component\Config\BaseConfigDefinition;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 
 class ConfigDefinition extends BaseConfigDefinition
 {
@@ -28,6 +29,25 @@ class ConfigDefinition extends BaseConfigDefinition
                     ->children()
                         ->scalarNode('branch')
                     ->end()
+                ->end()
+                ->children()
+                        ->scalarNode('username')
+                    ->end()
+                ->end()
+                    ->children()
+                        ->scalarNode('password')
+                    ->end()
+                ->end()
+                ->validate()
+                ->always(function ($item) {
+                    if ((empty($item['username']) && !empty($item['password']))
+                        || (!empty($item['username']) && empty($item['password']))
+                    ) {
+                        throw new InvalidConfigurationException('Both username and password has to be set.');
+                    }
+                    return $item;
+                })
+            ->end()
             ->end();
 
         /** @noinspection NullPointerExceptionInspection */

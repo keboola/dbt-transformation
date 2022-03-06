24f17b598164e2c61353cec75dede7d8b73d1cea
Výborný Adam <mail@adamvyborny.cz>
2022-03-06 22:33:29 +0100

    Extracted run dbt to separate class

--- src/Component.php
+++ src/Component.php
@@ -9,8 +9,6 @@ use DbtTransformation\DbtYamlCreateService\DbtSourceYamlCreateService;
 use Keboola\Component\BaseComponent;
 use Keboola\Component\UserException;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
 
 class Component extends BaseComponent
 {
@@ -47,31 +45,10 @@ class Component extends BaseComponent
 
         $this->cloneRepository($config, $gitRepositoryUrl);
 
-        $this->setProjectPath($dataDir, $gitRepositoryUrl);
+        $this->setProjectPath($dataDir);
         $this->createDbtYamlFiles($config);
 
-        $selectParameter = [];
-        $modelNames = $config->getModelNames();
-        if (!empty($modelNames)) {
-            $selectParameter = ['--select', ...$modelNames];
-        }
-
-        $dbtCommand = [
-            'dbt',
-            '--log-format',
-            'json',
-            '--warn-error',
-            'run',
-            ...$selectParameter,
-            '--profiles-dir',
-            sprintf('%s/.dbt/', $this->projectPath),
-        ];
-
-        try {
-            $this->runProcess($dbtCommand, $this->projectPath);
-        } catch (ProcessFailedException $e) {
-            throw new UserException($e->getMessage());
-        }
+        (new DbtRunService($this->projectPath))->run($config->getModelNames());
 
         if ($config->showSqls()) {
             $sqls = (new ParseLogFileService(sprintf('%s/logs/dbt.log', $this->projectPath)))->getSqls();
@@ -98,21 +75,9 @@ class Component extends BaseComponent
         return ConfigDefinition::class;
     }
 
-    /**
-     * @param string[] $command
-     */
-    protected function runProcess(array $command, string $cwd): Process
+    protected function setProjectPath(string $dataDir): void
     {
-        $process = new Process($command, $cwd);
-        $process->mustRun();
-
-        return $process;
-    }
-
-    protected function setProjectPath(string $dataDir, string $gitRepositoryUrl): void
-    {
-        $explodedUrl = explode('/', $gitRepositoryUrl);
-        $this->projectPath = sprintf('%s/%s', $dataDir, pathinfo(end($explodedUrl), PATHINFO_FILENAME));
+        $this->projectPath = sprintf('%s/%s', $dataDir, 'dbt-project');
     }
 
     protected function createDbtYamlFiles(Config $config): void

f83dd6357e52fdfbd109a19205e59fb3b02aea34
Výborný Adam <mail@adamvyborny.cz>
2022-06-16 12:42:55 +0200

    Removed libs and tests part which are no longer needed

--- tests/functional/DatadirTest.php
+++ tests/functional/DatadirTest.php
@@ -4,34 +4,8 @@ declare(strict_types=1);
 
 namespace DbtTransformation\FunctionalTests;
 
-use DbtTransformation\Traits\SnowflakeTestQueriesTrait;
 use Keboola\DatadirTests\DatadirTestCase;
-use RuntimeException;
 
 class DatadirTest extends DatadirTestCase
 {
-    use SnowflakeTestQueriesTrait;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $testProjectDir = $this->getTestFileDir() . '/' . $this->dataName();
-
-        $this->connection = OdbcTestConnection::createConnection();
-        $this->removeAllTablesAndViews();
-
-        // Load setUp.php file - used to init database state
-        $setUpPhpFile = $testProjectDir . '/setUp.php';
-        if (file_exists($setUpPhpFile)) {
-            // Get callback from file and check it
-            $initCallback = require $setUpPhpFile;
-            if (!is_callable($initCallback)) {
-                throw new RuntimeException(sprintf('File "%s" must return callback!', $setUpPhpFile));
-            }
-
-            // Invoke callback
-            $initCallback($this);
-        }
-    }
 }

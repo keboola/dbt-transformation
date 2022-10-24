dbcb15e3017c5e7fd8dc73a08d213bd2976e8d97
MiroCillik <mcillik@gmail.com>
2022-10-24 16:39:33 +0200

    refactor: ManifestConverter

--- src/FileDumper/OutputManifestJson.php
+++ src/FileDumper/OutputManifestJson.php
@@ -7,82 +7,21 @@ namespace DbtTransformation\FileDumper;
 class OutputManifestJson extends FilesystemAwareDumper
 {
     private string $outputDir;
-    private string $sourceManifestPath;
 
-    public function __construct(string $dataDir, string $projectPath)
+    public function __construct(string $dataDir)
     {
         parent::__construct();
         $this->outputDir = $dataDir . '/out/tables';
-        $this->sourceManifestPath = $projectPath . '/target/manifest.json';
     }
 
-    public function dumpJson(): void
+    /**
+     * @param array<string, array<int|string, mixed>> $manifestData
+     */
+    public function dumpJson(string $tableName, array $manifestData): void
     {
-        if (!file_exists($this->sourceManifestPath)) {
-            return;
-        }
-
-        $dbtManifest = (array) json_decode(
-            (string) file_get_contents($this->sourceManifestPath),
-            true,
-            512,
-            JSON_THROW_ON_ERROR
+        $this->filesystem->dumpFile(
+            sprintf('%s/%s.manifest', $this->outputDir, $tableName),
+            (string) json_encode(array_filter($manifestData))
         );
-
-        $modelNodes = array_filter($dbtManifest['nodes'], fn ($node) => $node['resource_type'] === 'model');
-
-        foreach ($modelNodes as $tableData) {
-            $tableMetadata = [];
-            if (isset($tableData['description'])) {
-                $tableMetadata[] = [
-                    'key' => 'description',
-                    'value' =>$tableData['description'],
-                ];
-            }
-            if (isset($tableData['meta']['owner'])) {
-                $tableMetadata[] = [
-                    'key' => 'meta.owner',
-                    'value' =>$tableData['meta']['owner'],
-                ];
-            }
-
-            $primaryKey = [];
-            $columnsMetadata = [];
-            foreach ($tableData['columns'] as $columnName => $values) {
-                if (isset($values['description'])) {
-                    $columnsMetadata[$columnName][] = [
-                        'key' => 'description',
-                        'value' => $values['description'],
-                    ];
-                }
-                if (isset($values['meta']['primary-key'])) {
-                    $columnsMetadata[$columnName][] = [
-                        'key' => 'meta.primary-key',
-                        'value' => $values['meta']['primary-key'],
-                    ];
-                    if ($values['meta']['primary-key'] === true) {
-                        $primaryKey[] = $columnName;
-                    }
-                }
-                if (isset($values['data_type'])) {
-                    $columnsMetadata[$columnName][] = [
-                        'key' => 'data_type',
-                        'value' => $values['data_type'],
-                    ];
-                }
-            }
-
-            $manifestData = [
-                'columns' => array_keys($tableData['columns']),
-                'primary_key' => $primaryKey,
-                'metadata' => $tableMetadata,
-                'column_metadata' => $columnsMetadata,
-            ];
-
-            $this->filesystem->dumpFile(
-                sprintf('%s/%s.manifest', $this->outputDir, $tableData['name']),
-                (string) json_encode(array_filter($manifestData))
-            );
-        }
     }
 }
--- tests/phpunit/FileDumper/OutputManifestJsonTest.php
+++ tests/phpunit/FileDumper/OutputManifestJsonTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace DbtTransformation\Tests\FileDumper;
 
+use DbtTransformation\FileDumper\OutputManifest\ManifestConverter;
 use DbtTransformation\FileDumper\OutputManifestJson;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Filesystem\Filesystem;
@@ -23,25 +24,15 @@ class OutputManifestJsonTest extends TestCase
 
     public function testDumpJson(): void
     {
-        $outputManifest = new OutputManifestJson($this->dataDir, $this->providerDataDir);
-        $outputManifest->dumpJson();
+        $outputManifest = new OutputManifestJson($this->dataDir);
+        $manifestConverter = new ManifestConverter($this->providerDataDir);
+        foreach ($manifestConverter->toOutputTables() as $tableName => $manifestData) {
+            $outputManifest->dumpJson($tableName, $manifestData);
+        }
 
         $tableManifestPath1 = $this->dataDir . '/out/tables/beers_with_breweries.manifest';
         $tableManifestPath2 = $this->dataDir . '/out/tables/beers.manifest';
         self::assertFileExists($tableManifestPath1);
         self::assertFileExists($tableManifestPath2);
-
-        $manifest1 = json_decode((string) file_get_contents($tableManifestPath1), true);
-        $manifest2 = json_decode((string) file_get_contents($tableManifestPath2), true);
-
-        self::assertArrayHasKey('columns', $manifest1);
-        self::assertArrayHasKey('primary_key', $manifest1);
-        self::assertArrayHasKey('metadata', $manifest1);
-        self::assertArrayHasKey('column_metadata', $manifest1);
-
-        self::assertArrayHasKey('columns', $manifest2);
-        self::assertArrayHasKey('primary_key', $manifest2);
-        self::assertArrayHasKey('metadata', $manifest2);
-        self::assertArrayHasKey('column_metadata', $manifest2);
     }
 }

1a52c9da931cc83545cf77bd4519acfabd475912
MiroCillik <mcillik@gmail.com>
2026-03-17 21:07:01 +0100

    try: snowflake-connector-python < 4

--- src/DwhProvider/RemoteSnowflakeProvider.php
+++ src/DwhProvider/RemoteSnowflakeProvider.php
@@ -41,8 +41,7 @@ class RemoteSnowflakeProvider extends RemoteProvider implements DwhProviderInter
         putenv(sprintf('DBT_KBC_PROD_SCHEMA=%s', $workspace['schema']));
         putenv(sprintf('DBT_KBC_PROD_DATABASE=%s', $workspace['database']));
         putenv(sprintf('DBT_KBC_PROD_WAREHOUSE=%s', $workspace['warehouse']));
-//        $account = str_replace(LocalSnowflakeProvider::STRING_TO_REMOVE_FROM_HOST, '', $workspace['host']);
-        $account = $workspace['host'];
+        $account = str_replace(LocalSnowflakeProvider::STRING_TO_REMOVE_FROM_HOST, '', $workspace['host']);
         putenv(sprintf('DBT_KBC_PROD_ACCOUNT=%s', $account));
         putenv(sprintf('DBT_KBC_PROD_USER=%s', $workspace['user']));
 

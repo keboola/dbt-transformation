55795411b8df9190aa1294dbab3b81539f80b351
MiroCillik <mcillik@gmail.com>
2026-03-17 21:27:09 +0100

    try: set host

--- src/DwhProvider/RemoteSnowflakeProvider.php
+++ src/DwhProvider/RemoteSnowflakeProvider.php
@@ -41,6 +41,7 @@ class RemoteSnowflakeProvider extends RemoteProvider implements DwhProviderInter
         putenv(sprintf('DBT_KBC_PROD_SCHEMA=%s', $workspace['schema']));
         putenv(sprintf('DBT_KBC_PROD_DATABASE=%s', $workspace['database']));
         putenv(sprintf('DBT_KBC_PROD_WAREHOUSE=%s', $workspace['warehouse']));
+        putenv(sprintf('DBT_KBC_PROD_HOST=%s', $workspace['host']));
         $account = str_replace(LocalSnowflakeProvider::STRING_TO_REMOVE_FROM_HOST, '', $workspace['host']);
         putenv(sprintf('DBT_KBC_PROD_ACCOUNT=%s', $account));
         putenv(sprintf('DBT_KBC_PROD_USER=%s', $workspace['user']));
@@ -67,6 +68,7 @@ class RemoteSnowflakeProvider extends RemoteProvider implements DwhProviderInter
             'database',
             'account',
             'threads',
+            'host'
         ];
 
         if (getenv('DBT_KBC_PROD_PRIVATE_KEY') !== false) {
